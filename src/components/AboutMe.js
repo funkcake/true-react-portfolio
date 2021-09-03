2be28b97b3af9d15cdc2654af15dfa41c44a234d
funkcake <castobrianj@gmail.com>
2021-09-03 14:49:26 -0500

    changed formating on AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,9 +6,9 @@ const AboutMe = () => {
         <main>
             <img src={background} alt="" className="absolute object-cover w-full h-full"
             />
-            <section className="relative flex justify-center pt-12 lg:pt-44 mx-auto">
-            <h1><img className="photo" src={me} alt=""/></h1>
-            <h1 className="text-6xl text-black-200 font-bold cursive leading-none lg:leading-snug home-name">Brian Casto</h1>
+            <section className="relative grid grid-cols-2 pt-12 lg:pt-44 mx-auto">
+            <p className=""><img className="pic" src={me} alt=""/></p>
+            <h1 className="text-4xl md:text-9xl text-left align-text-bottom text-black-200 font-bold cursive leading-none lg:leading-snug">Brian Casto</h1>
             </section>
             <section className="relative flex w-6/12 justify-center content-center mx-auto font-bold">
             <h4>Welcome to my portfolio. I am exited to get to work and ready to help make your dreams become my code. Some of the coding languages that are familiar to me are java, html, javascript, c++, c#, and python. Entering into coding has always been something I have wanted to do. Playing around with code since my high school day's making calculators and simple games. To making websites and apps that can help make peoples lives better.</h4>
